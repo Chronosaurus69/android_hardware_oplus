d59e4d32898b84efeef344f8f6e9154ae2be6363
xDanElisia <32947233+Chronosaurus69@users.noreply.github.com>
2026-02-24 00:35:31 +0700

    Update Android.mk

--- audio_amplifier/Android.mk
+++ audio_amplifier/Android.mk
@@ -20,7 +20,7 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_EXT_AMPLIFIER)),true)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := audio_amplifier.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := audio_amplifier.kona(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := audio_amplifier.c
 LOCAL_VENDOR_MODULE := true
